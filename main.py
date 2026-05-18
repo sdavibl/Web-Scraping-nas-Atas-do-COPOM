c7bb6104a737ec5088f2e7cada151c1f91bda8ec
Salomão Davi <salomaodavi1889@gmail.com>
2026-05-18 16:55:25 -0300

    Esqueleto do Seleniu
    Não foi possível extrair informações do site pelo BS4, tentarei pelo Sellenium

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import pandas as pd
+import numpy as np
 from bs4 import BeautifulSoup
 import requests
+import spacy
+import re
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+
+# options.add_argument('--headless')
+
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://www.bcb.gov.br/publicacoes/atascopom")
+
+print(driver.title)
+
+driver.quit()
+
 
 from bs4 import BeautifulSoup
 with requests.get("https://www.bcb.gov.br/publicacoes/atascopom") as req:
