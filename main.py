c4fd22434cd91750f97256166da0be74a92e7f82
Salomão Davi <salomaodavi1889@gmail.com>
2026-05-19 14:42:55 -0300

    feat: scraping automatizado das atas do COPOM
    - Abre as últimas 47 atas do COPOM
    - Extrai data e conteúdo
    - Salva em um arquivo CSV

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import pandas as pd
 import numpy as np
-from bs4 import BeautifulSoup
-import requests
-import spacy
-import re
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from nlp import lemmetizador
-from driver import criar_driver, selecionar_elemento
+from driver import criar_driver
 from scraper_bcb import scraping
+
 
 def main():
     with criar_driver() as driver:
-        scraping(driver)
-    return 0
+        datas, conteudos = scraping(driver)
+    
+    df = pd.DataFrame({
+            'data':datas,
+            'texto':conteudos
+    })
+
+    df.to_csv("atas_copom.csv")
 
 if __name__=="__main__":
     main()
--- scraper_bcb.py
+++ scraper_bcb.py
@@ -2,31 +2,44 @@ from driver import selecionar_elemento, selecionar_elementos
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import pandas as pd
+import numpy as np
+from time import sleep
+from numpy.random import rand, randn, randint
 
 
-def scrapar_ata(driver, elemento):
-    ver_mais = selecionar_elemento(elemento, '.btn-vermais')
-    driver.execute_script("arguments[0].click()", ver_mais)
+
+def scrapar_ata(driver, url):
+    driver.get(url)
     
-    texto_ata = selecionar_elemento(driver, '#ataconteudo').text
+    selecionar_elemento(driver, '#ataconteudo h3')
+    texto_ata = driver.execute_script("""
+                                        return [...document.querySelectorAll('#ataconteudo p')].map(p=>p.textContent).join('\\n')
+                                        """)
     data_ata = selecionar_elemento(driver, 'div.informacoes span').text
     
     return data_ata, texto_ata
 
 
-def scraping(driver, n=20):
+def scraping(driver):
 
     """ Aqui basicamente ele vai iterar sobre cada publicação do BACEN. Coloquei somente as 20 últimas publicações. """
 
     driver.get("https://www.bcb.gov.br/publicacoes/atascopom/cronologicos")
     atas_cupom = selecionar_elementos(driver, ".resultados-relacionados > div")
+    urls = [selecionar_elemento(ata, '.btn-vermais').get_attribute('href') for ata in atas_cupom]
 
     datas = []
     conteudos = []
 
-    for ata in atas_cupom[0:n]:
-        data, texto = scrapar_ata(driver, ata)
+    # Antes de junho de 2020 só teremos os textos em PDF, que iremos extrair o texto em um projeto futuro
+    for url in urls[0:46]:
+        data, texto = scrapar_ata(driver, url)
         datas.append(data)
         conteudos.append(texto)
-        driver.get("https://www.bcb.gov.br/publicacoes/atascopom/cronologicos")
-    return 0
+
+        # Dorme a execução por um tempo normalmente distribuído com média 7 e dp = 3
+        tempo = abs(3*randn() + 7)
+        sleep(tempo)
+        
+    return datas, conteudos
